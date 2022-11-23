e69782d3dd8d5eb2fc47909660693de2c365065d
Martin Yip <martinyip220@gmail.com>
2022-11-23 00:11:15 -0800

    created get details from spot id router and updated alias to Owner in spots model

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
-const { User, Spot, Review, ReviewImage, SpotImage, Booking, Sequelize } = require("../../db/models");
+const { User, Spot, Review, ReviewImage, SpotImage, Booking, Sequelize, sequelize } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
@@ -115,5 +115,61 @@ router.get('/current', requireAuth, async (req, res) => {
   res.json({"Spots": spotsInfo})
 })
 
+// Get details for a Spot from an id
+
+router.get('/:spotId', async (req, res) => {
+  const spot = await Spot.findByPk(req.params.spotId);
+
+  if (!spot) {
+    res.status(404);
+    res.json({
+      message: "Spot couldn't be found",
+      statusCode: 404
+    });
+  }
+
+  const reviewData = await spot.getReviews({
+    attributes: [[Sequelize.fn("AVG", Sequelize.col("stars")), "avgStarRating"]],
+  });
+
+  const avgStarRating = reviewData[0].toJSON().avgStarRating;
+
+  const numReviews = await Review.count({
+    where: {
+      spotId: spot.id
+    }
+  });
+
+  const spotImages = await SpotImage.findAll({
+    where: {
+      spotId: spot.id
+    },
+    attributes: ['id', 'url', 'preview']
+  });
+
+  const owner = await User.findByPk(spot.ownerId, {
+    attributes: ['id', 'firstName', 'lastName']
+  });
+
+  res.json({
+    id: spot.id,
+      ownerId: spot.ownerId,
+      address: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+      numReviews: numReviews,
+      avgStarRating: avgStarRating,
+      SpotImages: spotImages,
+      Owner: owner
+  })
+});
 
 module.exports = router;

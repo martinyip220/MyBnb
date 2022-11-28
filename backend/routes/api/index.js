4bbf67cfde61c35f215c37d28eb44c4302d7dce1
Martin Yip <martinyip220@gmail.com>
2022-11-28 01:20:19 -0800

    delete spot image done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
 const bookingRouter = require('./bookings.js');
+const spotImagesRouter = require('./spot-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -20,6 +21,8 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewRouter);
 
 router.use('/bookings', bookingRouter);
+
+router.use('/spot-images', spotImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });

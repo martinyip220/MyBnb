bcaf2f83bf157738b981c61e2ede853f15eb1773
Martin Yip <martinyip220@gmail.com>
2022-12-18 00:19:46 -0800

    added seeders and it populates on localhost

--- backend/db/seeders/20221122173825-demo-SpotImages.js
+++ backend/db/seeders/20221122173825-demo-SpotImages.js
@@ -35,6 +35,71 @@ module.exports = {
         url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-7615380/original/232a2ebb-4d4e-4b3e-8fee-1de0929f710c.jpeg?im_w=1200',
         preview: true
       },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/113bd9ea-b92c-4ab1-81cd-13825260e442.jpg?im_w=960',
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/65a7036b-4583-4992-b9a9-36fccb56ddae.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/c273ec5c-88c5-40cd-9b63-32d308ec70be.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: 'https://a0.muscache.com/im/pictures/444a8225-e657-4d62-97db-42f7423ae890.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 10,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-43316780/original/e5f00c7b-ed64-4d88-b386-631ca1d42b92.jpeg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 11,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53113872/original/f82e4714-6895-4e49-a1af-deefac0f1b33.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 12,
+        url: 'https://a0.muscache.com/im/pictures/1ef9b49c-6f99-4018-95f9-8471a9fbbd15.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 13,
+        url: 'https://a0.muscache.com/im/pictures/4383c750-0314-4342-9db4-4faa20cc7f44.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 14,
+        url: 'https://a0.muscache.com/im/pictures/532914d0-51d6-4d83-a0f9-c41eda4dc17a.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 15,
+        url: 'https://a0.muscache.com/im/pictures/0b405683-26b9-448f-9eb1-d4d2c58f4d40.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: 16,
+        url: 'https://a0.muscache.com/im/pictures/2adf6ef9-e131-431b-a34e-9566e768f509.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 17,
+        url: 'https://a0.muscache.com/im/pictures/f192135e-4df8-4d86-9cda-8e4c72fc5d93.jpg?im_w=720',
+        preview: true
+      },
+      {
+        spotId: 18,
+        url: 'https://a0.muscache.com/im/pictures/01ddf5ae-67a9-45ff-b068-a3ad4b68175e.jpg?im_w=720',
+        preview: true
+      },
     ], {});
   },
 
@@ -42,7 +107,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3, 4, 5] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] }
     }, {});
   }
 };
